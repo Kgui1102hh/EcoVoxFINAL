eeb96127d64c778ef237c2f454fb2a74b6372c05
Kgui1102hh <rm84128@estudante.fieb.edu.br>
2023-11-10 06:38:56 -0300

    mysql conect

--- config/connection-factory.cjs
+++ config/connection-factory.cjs
@@ -1,11 +1,11 @@
-var mysql = require("mysql");
+var mysql = require("mysql2");
 
 module.exports = function(){
     return mysql.createConnection({
-        host: "localhost",
+        host: "viaduct.proxy.rlwy.net",
         user: "root",
-        password: "17022006",
-        database: "ecovox",
-        port: 3306
+        password: "cdAe2hAcFadcEa65CCCGda52AeAHBc1A",
+        database: "railway",
+        port: 13385
     })
 }
